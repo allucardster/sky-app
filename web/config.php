c29ff2bac8e0f0dc80207e79f89907ebf0988dc4
Richard Melo <allucardster@gmail.com>
2023-03-31 17:17:57 -0500

    Allow access to config frontal controller from `localhost`

--- web/config.php
+++ web/config.php
@@ -4,10 +4,9 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit("This script cannot be run from the CLI. Run it from a browser.\n");
 }
 
-if (!in_array(@$_SERVER['SERVER_NAME'], array(
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-    'localhost'
 ))) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
