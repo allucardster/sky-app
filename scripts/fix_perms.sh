6374ddf42d10903e95191945eac496aadfccd766
Richard Melo <allucardster@gmail.com>
2023-03-31 15:37:44 -0500

    Initial commit

---
+++ scripts/fix_perms.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
+setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX app/cache app/logs
+setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX app/cache app/logs
